edb8515aa6fb2738ab53bb20ac830bb5f6aa1c95
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-24 20:37:53 +0800

    add ant design

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import { store } from './store/index'
 import router from './router'
 
-Vue.config.productionTip = false
+import Antd from "ant-design-vue"
+import 'ant-design-vue/dist/antd.css';
+
+
+
+Vue.use(Antd);
+Vue.config.productionTip = false;
+
+
 
 new Vue({
     store,
